aa3760cb6a7b9c051e682112547c0cffa108262a
Samuil Gospodinov <samuil4@gmail.com>
2018-09-02 23:08:25 +0300

    updated ng-sq-typeahead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,27 @@ import { LabelValuePair, SearchResult } from 'ng-sq-ui';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   testForm: FormGroup;
   showModal = false;
-  searchResults: SearchResult[];
+  searchResults: any[];
+  searchResultsStrings: string[];
 
   dropdownOptions: LabelValuePair[] = [
     {
       label: 'option1',
-      value: 'someVal1'
+      value: 'someVal1',
     },
     {
       label: 'option2',
-      value: 'someVal2'
+      value: 'someVal2',
     },
     {
       label: 'option3',
-      value: 'someVal3'
-    }
+      value: 'someVal3',
+    },
   ];
 
   constructor(private fb: FormBuilder) {
@@ -32,28 +33,53 @@ export class AppComponent {
       name: ['', Validators.required],
       dropdown: [null, Validators.required],
       tags: [['tag1'], Validators.required],
-      typeahead: [[], Validators.required],
+      typeahead1: [[], Validators.required],
+      typeahead2: [[], Validators.required],
       radioValue: ['value1'],
-      checkboxValue: [false]
+      checkboxValue: [false],
     });
   }
 
   searchMethod(query) {
     this.searchResults = [
       {
-        displayName: 'Search result 1',
-        value: 1
+        label: 'option1',
+        value: 'someVal1',
+        prop: 1,
+        uid: 12,
+        nested: {
+          level2: {
+            prop: '1',
+          },
+        },
       },
       {
-        displayName: 'Search result 2',
-        value: 2
-      }
-      ,
+        label: 'option2',
+        value: 'someVal2',
+        prop: 2,
+        uid: 22,
+        nested: {
+          level2: {
+            prop: '2',
+          },
+        },
+      },
       {
-        displayName: 'Search result 3',
-        value: 3
-      }
+        label: 'option3',
+        value: 'someVal3',
+        prop: 3,
+        uid: 32,
+        nested: {
+          level2: {
+            prop: '1',
+          },
+        },
+      },
     ];
+  }
+
+  searchMethodString(query) {
+    this.searchResultsStrings = ['option1', 'option2', 'option3', 'option4'];
   }
 
   onSubmit() {
