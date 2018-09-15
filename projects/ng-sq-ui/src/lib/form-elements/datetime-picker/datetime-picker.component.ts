1fd70ae342121a77f99f8141b9a9e5666c661fc9
Plamena Radneva <plamena.radneva@mentormate.com>
2018-09-15 23:58:02 +0300

    some more enhancements

--- projects/ng-sq-ui/src/lib/form-elements/datetime-picker/datetime-picker.component.ts
+++ projects/ng-sq-ui/src/lib/form-elements/datetime-picker/datetime-picker.component.ts
@@ -13,13 +13,20 @@ const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR = {
 
 export interface CalendarDay {
   displayDate: string;
-  isBeyondCurrentMonth: boolean;
+  relativityToCurrentMonth: CalendarPeriodRelativity;
   isSelected: boolean;
+  isDisabled: boolean;
 }
 
-export enum PeriodType {
+export enum CalendarPeriodType {
   Month = 0,
   Year
+}
+
+export enum CalendarPeriodRelativity {
+  Current = 0,
+  Before,
+  After
 }
 
 @Component({
@@ -31,34 +38,67 @@ export enum PeriodType {
 })
 export class DatetimePickerComponent extends InputCoreComponent implements OnInit {
   @Input() locale: string = 'en';
+  @Input() maxDate: momentNs.Moment | Date;
+  @Input() minDate: momentNs.Moment | Date;
 
   weekdays: string[];
-  currentMonth = moment();
-  period: PeriodType = PeriodType.Month;
+  currentMonth: momentNs.Moment;
+  period: CalendarPeriodType = CalendarPeriodType.Month;
   table = [];
+  calendarPeriodRelativity = CalendarPeriodRelativity;
 
   private previouslySelected: CalendarDay;
+  private chosenDate: momentNs.Moment;
 
   constructor() {
     super();
   }
 
   ngOnInit() {
+    this.value = moment().locale(this.locale);
+    this.currentMonth = moment().locale(this.locale);
+
     moment.locale(this.locale);
     this.weekdays = this.getWeekdays();
     this.table = this.getMonthCalendar(moment().locale(this.locale));
   }
 
+  onDateClick(date: CalendarDay) {
+    switch (date.relativityToCurrentMonth) {
+      case CalendarPeriodRelativity.After:
+        this.select(date);
+        this.next();
+        break;
+      case CalendarPeriodRelativity.Before:
+        this.select(date);
+        this.previous();
+        break;
+      default:
+        this.select(date);
+        break;
+    }
+  }
+
   select(date: CalendarDay) {
+    const month = this.currentMonth.clone();
+
+    if (date.relativityToCurrentMonth === CalendarPeriodRelativity.Before) {
+      month.subtract(1, 'month');
+    }
+
+    if (date.relativityToCurrentMonth === CalendarPeriodRelativity.After) {
+      month.add(1, 'month');
+    }
+
     this.value = moment()
-      .year(this.currentMonth.year())
-      .month(this.currentMonth.month())
+      .year(month.year())
+      .month(month.month())
       .date(parseInt(date.displayDate, 10))
       .locale(this.locale);
 
     date.isSelected = true;
 
-    if (this.previouslySelected) {
+    if (this.previouslySelected && !Object.is(date, this.previouslySelected)) {
       this.previouslySelected.isSelected = false;
     }
 
@@ -66,16 +106,16 @@ export class DatetimePickerComponent extends InputCoreComponent implements OnIni
   }
 
   next() {
-    if (this.period === PeriodType.Month) {
+    if (this.period === CalendarPeriodType.Month) {
       const nextMonth = this.currentMonth.add(1, 'month');
       this.table = this.getMonthCalendar(nextMonth);
     }
   }
 
   previous() {
-    if (this.period === PeriodType.Month) {
+    if (this.period === CalendarPeriodType.Month) {
       const previousMonth = this.currentMonth.subtract(1, 'month');
-      this.table  = this.getMonthCalendar(previousMonth);
+      this.table = this.getMonthCalendar(previousMonth);
     }
   }
 
@@ -85,12 +125,12 @@ export class DatetimePickerComponent extends InputCoreComponent implements OnIni
 
   getMonthCalendar(period) {
     const monthStart = moment(period).startOf('month').locale(this.locale);
-    const tableEnd = moment(monthStart).add(1, 'month').locale(this.locale);
     const isStartOfChosenMonthTheFirstDayOfTable = (monthStart.weekday() === 0);
 
     const dateIterator = monthStart.clone();
     const calendar: Array<CalendarDay[]> = [];
     let tableRow: CalendarDay[] = [];
+    let newDate: CalendarDay;
 
     if (!isStartOfChosenMonthTheFirstDayOfTable) {
       let daysToGoBack = dateIterator.weekday();
@@ -98,22 +138,25 @@ export class DatetimePickerComponent extends InputCoreComponent implements OnIni
       dateIterator.subtract(daysToGoBack, 'days');
     }
 
-    while (dateIterator.isSameOrBefore(tableEnd) || calendar.length < 6) {
+    // add dates until the calendar has 6 week rows
+    while (calendar.length < 6) {
+      newDate = {
+        displayDate: dateIterator.format('D'),
+        relativityToCurrentMonth: this.determineDateRelativityToCurrentMonth(dateIterator),
+        isDisabled: this.determineIfDateIsDisabled(dateIterator),
+        isSelected: moment(this.value).isSame(dateIterator, 'day')
+      };
+
+      if (newDate.isSelected) {
+        this.previouslySelected = newDate;
+      }
+
       if (tableRow.length <= 6) {
-        tableRow.push({
-          displayDate: dateIterator.format('D'),
-          isBeyondCurrentMonth: this.isDateBeyondCurrentMonth(dateIterator),
-          isSelected: false
-        });
+        tableRow.push(newDate);
 
       } else {
         calendar.push(tableRow);
-
-        tableRow = [{
-          displayDate: dateIterator.format('D'),
-          isBeyondCurrentMonth: this.isDateBeyondCurrentMonth(dateIterator),
-          isSelected: false
-        }];
+        tableRow = [newDate];
       }
 
       dateIterator.add(1, 'day');
@@ -122,12 +165,26 @@ export class DatetimePickerComponent extends InputCoreComponent implements OnIni
     return calendar;
   }
 
-  private isDateBeyondCurrentMonth(date): boolean {
+  private determineIfDateIsDisabled(date: momentNs.Moment): boolean {
+    const isAfterMaxDate = this.maxDate && date.isAfter(this.maxDate);
+    const isBeforeMinDate = this.minDate && date.isBefore(this.minDate);
+
+    return isAfterMaxDate || isBeforeMinDate;
+  }
+
+  private determineDateRelativityToCurrentMonth(date: momentNs.Moment): CalendarPeriodRelativity {
     const startOfCurrentMonth = moment(this.currentMonth).startOf('month');
     const endOfCurrentMonth = moment(this.currentMonth).endOf('month');
 
-    return (moment(date).isBefore(startOfCurrentMonth) ||
-            moment(date).isAfter(endOfCurrentMonth));
+    if (moment(date).isBefore(startOfCurrentMonth)) {
+      return CalendarPeriodRelativity.Before;
+    }
+
+    if (moment(date).isAfter(endOfCurrentMonth)) {
+      return CalendarPeriodRelativity.After;
+    }
+
+    return CalendarPeriodRelativity.Current;
   }
 
 }
