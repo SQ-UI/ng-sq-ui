4ef4e9e5cfe5cc25a2e6c7f51273af618df8e162
Plamena Radneva <rhythmxholic@gmail.com>
2018-09-26 22:42:13 +0300

    timepicker started

---
+++ projects/ng-sq-ui/src/lib/datetime-picker/time-picker/time-picker.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'sq-time-picker',
+  templateUrl: './time-picker.component.html',
+  styleUrls: ['./time-picker.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TimePickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
