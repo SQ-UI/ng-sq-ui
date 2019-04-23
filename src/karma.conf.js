d224b5c34915d95c1661fe54bef8ba698e8abbf2
Samuil Gospodinov <samuil4@gmail.com>
2019-04-23 13:30:38 +0300

    updated test coverage reports

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,7 +20,9 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: config.angularCli && config.angularCli.codeCoverage
+    ? ['progress', 'coverage-istanbul']
+    : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
