5fa16cb3129e61c1199fb914704ae38c6d2638c3
ardentia <rhythmxholic@gmail.com>
2021-02-14 20:48:26 +0200

    fix codeclimate yml tresholds and configs

--- projects/ng-sq-common/src/lib/entities/input-core-component.ts
+++ projects/ng-sq-common/src/lib/entities/input-core-component.ts
@@ -5,7 +5,6 @@ import { ControlValueAccessorEnabler } from './control-value-accessor-enabler';
  * This class contains all the base properties
  * needed for every possible SQ UI form control
  **/
-@Directive()
 export class InputCoreComponent extends ControlValueAccessorEnabler {
   private defaultInputIdentifier = 'sq-form-control' + new Date().getTime().toString();
 
