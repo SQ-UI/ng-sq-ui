6dd77b476e01c652a868c8779918781a6f036e2f
ardentia <rhythmxholic@gmail.com>
2021-05-05 18:25:01 +0300

    created more examples for custom templates

--- projects/ng-sq-common/src/lib/entities/input-core-component.ts
+++ projects/ng-sq-common/src/lib/entities/input-core-component.ts
@@ -1,10 +1,11 @@
-import { Input, Directive } from '@angular/core';
+import { Input, Directive, Injectable } from '@angular/core';
 import { ControlValueAccessorEnabler } from './control-value-accessor-enabler';
 
 /**
  * This class contains all the base properties
  * needed for every possible SQ UI form control
  **/
+@Injectable()
 export class InputCoreComponent extends ControlValueAccessorEnabler {
   private defaultInputIdentifier = 'sq-form-control' + new Date().getTime().toString();
 
