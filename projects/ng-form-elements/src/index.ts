2fde2bebe390e7b496a4c9c55fe111d767f40581
ardentia <rhythmxholic@gmail.com>
2022-02-23 17:21:07 +0200

    separate form-elements and progress-bar

---
+++ projects/ng-form-elements/src/index.ts
@@ -0,0 +1,20 @@
+/*
+ * Public API Surface of form-elements
+ */
+
+export * from './lib/ng-form-elements.module';
+export { NgFormElementsModule } from './lib/ng-form-elements.module';
+export { InputComponent } from './lib/input/input.component';
+export { DropdownComponent } from './lib/dropdown/dropdown.component';
+export { TagsInputComponent } from './lib/tags-input/tags-input.component';
+export { RadiobuttonComponent } from './lib/radiobutton/radiobutton.component';
+export { FormGroupComponent } from './lib/form-group/form-group.component';
+export { TypeaheadComponent } from './lib/typeahead/typeahead.component';
+export { CheckboxComponent } from './lib/checkbox/checkbox.component';
+export { ButtonComponent } from './lib/button/button.component';
+export { TextareaComponent } from './lib/textarea/textarea.component';
+export { SqCheckboxLabelTemplateDirective } from './lib/checkbox/checkbox.template.directive';
+export { SqDropdownOptionTemplateDirective, SqDropdownChevronTemplateDirective, SqDropdownSelectedOptionTemplateDirective } from './lib/dropdown/dropdown.template.directive';
+export { SqRadiobuttonLabelTemplateDirective } from './lib/radiobutton/radiobutton.template.directive';
+export { SqTagTemplateDirective } from './lib/tags-input/tags-input.template.directive';
+export { SqTypeaheadOptionTemplateDirective, SqTypeaheadSelectedOptionTemplateDirective } from './lib/typeahead/typeahead.template.directive';
---
+++ projects/ng-progress-bar/README.md
@@ -0,0 +1,24 @@
+# ProgressBar
+
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.0.
+
+## Code scaffolding
+
+Run `ng generate component component-name --project progress-bar` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project progress-bar`.
+> Note: Don't forget to add `--project progress-bar` or else it will be added to the default project in your `angular.json` file. 
+
+## Build
+
+Run `ng build progress-bar` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Publishing
+
+After building your library with `ng build progress-bar`, go to the dist folder `cd dist/progress-bar` and run `npm publish`.
+
+## Running unit tests
+
+Run `ng test progress-bar` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
