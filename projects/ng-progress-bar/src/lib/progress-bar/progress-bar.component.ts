89e98c566600b65680639d5acca92da399807373
mcode-bot@modelcode.ai <mcode-bot@modelcode.ai>
2026-07-26 12:56:33 +0000

    feat(ng-progress-bar): migrate to standalone component with signal-based APIs

--- projects/ng-progress-bar/src/lib/progress-bar/progress-bar.component.ts
+++ projects/ng-progress-bar/src/lib/progress-bar/progress-bar.component.ts
@@ -1,28 +1,20 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
-import { ProgressBarSize, Size } from '@sq-ui/ng-sq-common';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, input } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Size } from '@sq-ui/ng-sq-common';
 
 @Component({
   selector: 'sq-progress-bar',
+  standalone: true,
+  imports: [NgClass, NgStyle],
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProgressBarComponent implements OnInit, ProgressBarSize {
-  @Input()
-  size: Size;
-  @Input()
-  loaded: number;
-  @Input()
-  infinite = false;
-
-  @Input() backgroundColor = '';
-  @Input() fillColor = '';
-
-  constructor() {}
-
-  ngOnInit() {
-    if (!this.size) {
-      this.size = 'medium';
-    }
-  }
+export class ProgressBarComponent {
+  size = input<Size>('medium');
+  loaded = input<number>();
+  infinite = input<boolean>(false);
+  backgroundColor = input<string>('');
+  fillColor = input<string>('');
 }
